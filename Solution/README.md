5a1c5f3ee7352809df55feb3569c684371d82872
Igneaalis <Nostaleal.ru@yandex.ru>
2021-02-05 15:39:56 +0400

    3. Longest Substring Without Repeating Characters

--- Solution/README.md
+++ Solution/README.md
@@ -1,1 +1,70 @@
 ﻿# Leetcode-CSharp solutions by Igneaalis
+
+[3. Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
+
+```C#
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        if (s.Length < 2)
+        {
+            return s.Length;
+        }
+
+        var result = new StringBuilder();
+        int maxLength = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (result.ToString().Contains(s[i]))
+            {
+                result.Clear();
+            }
+            result.Append(s[i]);
+            maxLength = result.Length > maxLength ? result.Length : maxLength;
+        }
+
+        if (maxLength == s.Length)
+        {
+            return maxLength;
+        }
+
+        string curSubstring;
+        for (int i = s.Length; i > 0 && maxLength < s.Length - 1; i--)
+        {
+            int prevLength = maxLength;
+            for (int j = 0; j < s.Length - maxLength; j++)
+            {
+                bool doBreak = false;
+                curSubstring = s.Substring(j, maxLength + 1);
+                for (int k = 1; k < curSubstring.Length; k++)
+                {
+                    if (!curSubstring.Substring(0, k).Contains(curSubstring[k]))
+                    {
+                        if (maxLength < k + 1)
+                        {
+                            if (++maxLength == curSubstring.Length)
+                            {
+                                doBreak = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (doBreak)
+                {
+                    break;
+                }
+            }
+            if (prevLength == maxLength)
+            {
+                return maxLength;
+            }
+        }
+        return maxLength;
+    }
+}
+```
--- Solution/Solution.cs
+++ Solution/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Leetcode
 {
@@ -10,13 +11,76 @@ namespace Leetcode
 
         public class Solution
         {
-            
+            public int LengthOfLongestSubstring(string s)
+            {
+                if (s.Length < 2)
+                {
+                    return s.Length;
+                }
+
+                var result = new StringBuilder();
+                int maxLength = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (result.ToString().Contains(s[i]))
+                    {
+                        result.Clear();
+                    }
+                    result.Append(s[i]);
+                    maxLength = result.Length > maxLength ? result.Length : maxLength;
+                }
+
+                if (maxLength == s.Length)
+                {
+                    return maxLength;
+                }
+
+                string curSubstring;
+                for (int i = s.Length; i > 0 && maxLength < s.Length - 1; i--)
+                {
+                    int prevLength = maxLength;
+                    for (int j = 0; j < s.Length - maxLength; j++)
+                    {
+                        bool doBreak = false;
+                        curSubstring = s.Substring(j, maxLength + 1);
+                        for (int k = 1; k < curSubstring.Length; k++)
+                        {
+                            if (!curSubstring.Substring(0, k).Contains(curSubstring[k]))
+                            {
+                                if (maxLength < k + 1)
+                                {
+                                    if (++maxLength == curSubstring.Length)
+                                    {
+                                        doBreak = true;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        if (doBreak)
+                        {
+                            break;
+                        }
+                    }
+                    if (prevLength == maxLength)
+                    {
+                        return maxLength;
+                    }
+                }
+                return maxLength;
+            }
         }
 
         #endregion
 
         static void Main(string[] args)
         {
+            Solution s = new();
+            var result = s.LengthOfLongestSubstring("dvdfddvssss");
+            Console.WriteLine(result.ToString());
             Console.ReadKey();
         }
     }
